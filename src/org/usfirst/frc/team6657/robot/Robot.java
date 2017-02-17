4b50aab8bface3f93ad80fd3f1ca178fd20dce51
Mitchell Nuckols <mitchnuckols@gmail.com>
2017-02-17 14:31:26 -0800

    Added autonomous routines

--- src/org/usfirst/frc/team6657/robot/Robot.java
+++ src/org/usfirst/frc/team6657/robot/Robot.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.Joystick.ButtonType;
@@ -13,7 +14,10 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class Robot extends SampleRobot {
-	RobotDrive myRobot = new RobotDrive(0, 2, 1, 3); // class that handles basic drive
+	//RobotDrive myRobot = new RobotDrive(0, 2, 1, 3); // class that handles basic drive
+	RobotDrive myRobot = new RobotDrive(0, 1);
+	
+	Jaguar box = new Jaguar(2);
 												// operations
 	Joystick js = new Joystick(0); // set to ID 1 in DriverStation
 	
@@ -51,30 +55,76 @@ public class Robot extends SampleRobot {
 		
 		myRobot.setInvertedMotor(RobotDrive.MotorType.kFrontLeft, true);
 		myRobot.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
-		myRobot.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
-		myRobot.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
+		//myRobot.setInvertedMotor(RobotDrive.MotorType.kRearLeft, true);
+		//myRobot.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
 	}
 	
 	
 	@Override
     public void autonomous() {
 		//Calibrate the gyro
-		gyro.calibrate();
+		//gyro.calibrate();
 		
 		//Many not need to reset at this point since we just calibrated it.
 		//gyro.reset();
 		
-    	while (isAutonomous() && isEnabled()) {
+		autoPath(3); // Indicates which path to take
+		
+    	/*while (isAutonomous() && isEnabled()) {
     		double angle = gyro.getAngle();
     		myRobot.drive(-0.3, -angle*Kp);
     		myRobot.setExpiration(3.0);
 
     		Timer.delay(0.01);
-    	}
+    	}*/
     	
     	//Free up the gyro once we are done with it.
     	gyro.free();
     }
+	
+	private void autoPath(int scenario) {
+		double acc = 0.0; // NOT a good idea. However, idk how to do it any other way
+		
+		while(isAutonomous() && isEnabled()) {
+			double angle = gyro.getAngle();
+		
+			switch(scenario) {
+			case 0: {
+				myRobot.drive(-0.4, 0.0); // Kinda straight, switch to gyro driving later
+				break;
+			}
+			case 1: {
+				if(acc <= 2.0) myRobot.drive(-0.4, -0.4);
+				else if(acc > 2.0) myRobot.drive(-0.4, 0.0);
+				break;
+			}
+			case 2: {
+				if(acc <= 2.0) myRobot.drive(-0.4, 0.0);
+				else if(acc > 2.0 && acc <= 10.0) myRobot.drive(-1.0, -1.0);
+				else if(acc > 10.0 && acc <= 15.0) myRobot.drive(-0.4, 1.0);
+				break;
+			}
+			case 3: {
+				if(acc <= 1.0) myRobot.drive(-0.9, -angle*Kp);
+					//{
+						/*if(acc <= 0.1)
+						{
+							myRobot.drive(-0.4, 0.0);
+						}
+						else
+						{
+							myRobot.drive(-0.9, -angle*Kp);
+						}
+					}*/
+				else myRobot.drive(0.0, 0.0);
+				break;
+			}
+			}
+			
+			acc += 0.005;
+			Timer.delay(0.005);
+		}
+	}
 	
 	@Override
 	public void disabled() {
@@ -126,7 +176,7 @@ public class Robot extends SampleRobot {
 					
 					System.out.println(Math.abs(realPos - initialPos));
 					
-					myRobot.drive(-0., 1.0);
+					myRobot.drive(-0.4, 1.0);
 					realPos = gyro.getAngle(); //get the current heading
 					
 					//System.out.println(rate);
@@ -134,11 +184,13 @@ public class Robot extends SampleRobot {
 					
 					System.out.println("While loop end");
 					System.out.println("====================================");
-					
+					if(js.getRawButton(7)) {
+						break;
+					}
 					Timer.delay(0.005);
 				}
 				
-				time = 0;
+				//time = 0;
 				
 			}
 			
@@ -148,17 +200,14 @@ public class Robot extends SampleRobot {
 				//Don't think we need this, might be causing an issue.
 				gyro.reset();
 				double angle = gyro.getAngle(); // get current heading
-	            myRobot.drive(-1.0, -angle*testKp); // drive towards heading 0
-	            Timer.delay(0.004);
-			}
-			/*if(js.getRawButton(5)) {
-				c.start();
+	            myRobot.drive(-0.4, -angle*testKp); // drive towards heading 0
+	            Timer.delay(0.005);
 			}
 			
-			if(js.getRawButton(6)) {
-				c.stop();
-			}*/
-			myRobot.drive(0.0, 0.0);
+			if(js.getRawButton(9)) {
+				box.set(0.4);
+			}
+			
 		}
 	}
 }
